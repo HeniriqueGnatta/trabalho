ae22f91066d0cb9aa20a43fb37fe1568b44523a3
HeniriqueGnatta <henriquegnatta@gmail.com>
2024-12-12 19:47:11 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/*
+* Autor: [Henrique Gnatta]
+* Linguagem: [C]
+* Data: 12/12/2024
+* Descrição: [utilizei if e variaveis enoperadores]
+* Funcionalidades: [é trasformar a temperatura de celsius em fahrenheitn e Kelvin]
+* Versão: [1.0]
+*/
 #include <stdio.h>
 
 int main() {
@@ -7,12 +15,10 @@ int A=0;//declara a variavel A float
   scanf("%f",&T);//armazena o valor digitado na variavel
   printf("1-Fahrenheit\n2-Kelvin\n");//printa se a pessoa quer Fahrenheit ou Kelvin
     scanf("%d",&A);//armazena o valor na variavel
-switch(A){ // inicio o switch case
-  case 1: //case 1
-    printf("A Temperatura em Fahrenheit é: %.2f",(T*1.8)+32);// printa a temperatura e uso uma mascara e faço a conta
-  break;//para parar a case 1
-  case 2://case 2
+  if(A==1){ // inicio o if se for == a A printar Fahrenheit
+    printf("A Temperatura em Fahrenheit é: %.2f",T*1.8+32);}// printa a temperatura e uso uma mascara e faço a conta
+  if(A==2){//// inicio o if se for == a A printar Kelvin
     printf("A Temperatura em Kelvin é: %.2f ",T+273.15);//printa a temperatura usa a mascara e executo a conta
-break;}//determina que terminou o switch case
+  }
   return 0;//para quando o codigo terminae retornar 0
 }
