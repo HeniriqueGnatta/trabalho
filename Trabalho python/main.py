76d1463e7d0bfce0d90f3a7994bea014772541a6
HeniriqueGnatta <henriquegnatta@gmail.com>
2024-12-12 19:49:56 -0300

    Update main.py

--- Trabalho python/main.py
+++ Trabalho python/main.py
@@ -1,4 +1,11 @@
-
+'''
+* Autor: [Henrique Gnatta]
+* Linguagem: [Python]
+* Data: 12/12/2024
+* Descrição: [Utilizei vetores funcoes e if]
+* Funcionalidades: [ele pergunta uma conta se voce acertar voce ganha um premio]
+* Versão: [1.0]
+'''
 from  quiz import Q # importa a funcao dos calculo
 Q()#define que a funcao vai ser executada
 
